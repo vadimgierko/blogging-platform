c14b113a8de84beff132dbe2894c8bd12ef9d5da
vadimgierko <85487884+vadimgierko@users.noreply.github.com>
2021-10-20 19:59:41 +0200

    BlogPage renders clicked article & changes link

--- src/components/BlogPage.js
+++ src/components/BlogPage.js
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import { ref, onValue } from "firebase/database";
 import { database } from '../firebase';
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
 
 export default function BlogPage({ blogKey }) {
 
+    let {path, url} = useRouteMatch();
+
     const [blog, setBlog] = useState(null);
+
+    const [currentArticleLink, setCurrentArticleLink] = useState(null);
+    const [currentArticle, setCurrentArticle] = useState(null);
+
+    function convertArticleTitleIntoLink(articleTitle) {
+        return ("/" + articleTitle.replace(/ /g, "-").toLowerCase());
+    }
 
     function fetchBlog() {
 
@@ -22,6 +34,9 @@ export default function BlogPage({ blogKey }) {
         });
     }
 
+    console.log(currentArticleLink);
+    console.log(currentArticle)
+
     return (
         <div>
             {
@@ -33,14 +48,34 @@ export default function BlogPage({ blogKey }) {
                         {
                             blog.articles ? (
                                 <div className="row">
-                                    <div className="col">Here will be clicked article</div>
+                                    <Switch>
+                                        <Route path={currentArticleLink}>
+                                            {
+                                                currentArticle && currentArticleLink ? (
+                                                    <div className="col">
+                                                        {/*<h1>{currentArticle.title}</h1>
+                                                        <hr />*/}
+                                                        <ReactMarkdown children={currentArticle.content} remarkPlugins={[remarkGfm]} />
+                                                    </div>
+                                                ) : null
+                                            }
+                                        </Route>
+                                    </Switch>
                                     <div className="col-4">
                                         <h5>Table of content</h5>
                                         <hr />
                                         <ul>
                                             {
                                                 Object.entries(blog.articles).map((article) =>
-                                                    <li key={article[0]}>{article[1].title}</li>
+                                                    <Link
+                                                        key={article[0]}
+                                                        to={url + convertArticleTitleIntoLink(article[1].title)}
+                                                        className="d-block"
+                                                        onClick={() => {
+                                                            setCurrentArticle(article[1]);
+                                                            setCurrentArticleLink(url + convertArticleTitleIntoLink(article[1].title))
+                                                        }}
+                                                    >{article[1].title}</Link>
                                                 )
                                             }
                                         </ul>
